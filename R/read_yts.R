ffe5e31e4236c38f67614d9d5e30d7fab5ad785e
muschellij2 <muschellij2@gmail.com>
2019-01-03 17:31:57 -0500

    updated code

--- R/read_yts.R
+++ R/read_yts.R
@@ -2,6 +2,7 @@
 #'
 #' @param ... Additional arguments to pass to
 #' \code{\link{read_csv}}
+#' @param verbose Should messages be printed?
 #'
 #' @return A \code{tibble}
 #' @export
@@ -9,9 +10,15 @@
 #' @importFrom readr read_csv problems stop_for_problems
 #' @examples
 #' yts = read_yts()
-read_yts = function(...) {
+read_yts = function(..., verbose = FALSE) {
   fname = jhu_file("Youth_Tobacco_Survey_YTS_Data.csv")
-  df = readr::read_csv(fname, ...)
+  func = identity
+  if (!verbose) {
+    func = suppressMessages
+  }
+  func( {
+    df = readr::read_csv(fname, ...)
+  })
   readr::stop_for_problems(df)
   return(df)
 }
