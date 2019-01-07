22a7294b7c54fdcfab8331dd0dd2d07c2a0d9b46
muschellij2 <muschellij2@gmail.com>
2019-01-07 09:37:09 -0500

    updated long

--- R/read_circulator.R
+++ R/read_circulator.R
@@ -16,3 +16,27 @@ read_circulator = function(...) {
 }
 
 
+#' @export
+#' @importFrom lubridate mdy
+#' @import tidyr dplyr stringr
+read_circulator_long = function(...) {
+  circ = read_circulator(...)
+  circ = mutate(circ, date = mdy(date))
+  # change colnames for reshaping
+  colnames(circ) =  colnames(circ) %>%
+    str_replace("Board", ".Board") %>%
+    str_replace("Alight", ".Alight") %>%
+    str_replace("Average", ".Average")
+
+  # make long
+  long = gather(circ, "var", "number",
+                starts_with("orange"),
+                starts_with("purple"), starts_with("green"),
+                starts_with("banner"))
+  # separate
+  long = separate(long, var, into = c("line", "type"),
+                  sep = "[.]")
+  return(long)
+
+}
+
